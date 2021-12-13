99c8c5724f9af3c3ec3c5db5b9e21eec9eb28263
Juan Jose Mayorga <tmaniacus@gmail.com>
2021-12-13 07:09:33 -0600

    you can get and save comments in local storage

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 import { Modal, Platform, StyleSheet, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Feed } from './src/screens/Feed';
 import { Comments } from './src/screens/Comments';
 
-const items = [
-  {id: 0, author: 'Bob Ross'},
-  {id: 1, author: 'Chuck Norris'},
-];
+const ASYNC_STORAGE_COMMENTS_KEY = 'ASYNC_STORAGE_COMMENTS_KEY';
 
 const platformVersion =
   Platform.OS === 'ios'
@@ -26,6 +24,27 @@ const App = () => {
   const [state, setState] = useState(initialState)
   const { commentsForItem, selectedItemId, showModal } = state;
 
+  useEffect(() => {
+    getComments()
+  }, [])
+
+  const getComments = async() => {
+    try {
+      const commentsForItem = await AsyncStorage.getItem(
+        ASYNC_STORAGE_COMMENTS_KEY,
+      )
+
+      setState({
+        ...state,
+        commentsForItem: commentsForItem
+          ? JSON.parse(commentsForItem)
+          : {}
+      })
+    } catch (error) {
+      console.log('Failed to load comments')
+    }
+  }
+
   const openCommentScreen = (id: any) => {
     setState({
       ...state,
@@ -42,6 +61,33 @@ const App = () => {
     })
   }
 
+  const onSubmitComment = async(text:string) => {
+    const comments = commentsForItem[selectedItemId!] || [];
+
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text],
+    }
+
+    setState({
+      ...state,
+      commentsForItem: updated,
+    })
+
+    try {
+      await AsyncStorage.setItem(
+        ASYNC_STORAGE_COMMENTS_KEY,
+        JSON.stringify(updated),
+      )
+    } catch (error) {
+      console.log('Failed to save comment',
+      text,
+      'for',
+      selectedItemId
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Feed
@@ -54,12 +100,12 @@ const App = () => {
         animationType='slide'
         onRequestClose={closeCommentScreen}
       >
-        <Comments
-          style={styles.container}
-          comments={commentsForItem[selectedItemId] || []}
-          onClose={closeCommentScreen}
-          onSubmitComment={() => console.log('onSubmitComment pressed!')}
-        />
+      <Comments
+        style={styles.container}
+        comments={commentsForItem[selectedItemId!] || []}
+        onClose={closeCommentScreen}
+        onSubmitComment={onSubmitComment}
+      />
       </Modal>
     </View>
   );
